0135ee65953eeff912dd70e3627e6d469974e186
Dimon <dimaslutskih@gmail.com>
2020-10-13 11:50:46 +0700

    Добавить класс CaffeineBeverage

---
+++ caffeinebeverage.cpp
@@ -0,0 +1,14 @@
+#include "caffeinebeverage.h"
+
+CaffeineBeverage::CaffeineBeverage()
+{
+
+}
+
+void CaffeineBeverage::boilWater(){
+    cout << "Boiling water" << endl;
+}
+
+void CaffeineBeverage::pourInCup(){
+    cout << "Pouring into cup" << endl;
+}
